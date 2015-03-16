c0eae24dea2de5ff32edde8cb7a5fda88be296a2
rlittle@roberts-air <rlittle@roberts-air>
2015-03-15 22:46:33 -0400

    moved RFC822 into user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 require 'digest/sha1'
-require 'RFC822'
+# require 'RFC822'
 
 
 class User < ActiveRecord::Base
-  include RFC822
-
+  #  include RFC822
+  RFC822_valid = begin
+    qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+    dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
+    atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' +
+      '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
+    quoted_pair = '\\x5c[\\x00-\\x7f]'
+    domain_literal = "\\x5b(?:#{dtext}|#{quoted_pair})*\\x5d"
+    quoted_string = "\\x22(?:#{qtext}|#{quoted_pair})*\\x22"
+    domain_ref = atom
+    sub_domain = "(?:#{domain_ref}|#{domain_literal})"
+    word = "(?:#{atom}|#{quoted_string})"
+    domain = "#{sub_domain}(?:\\x2e#{sub_domain})*"
+    local_part = "#{word}(?:\\x2e#{word})*"
+    addr_spec = Regexp.new("#{local_part}\\x40#{domain}", nil, 'n')
+    pattern = /\A#{addr_spec}\z/
+  end
+  
   has_and_belongs_to_many :roles
   
   has_one :user_attribute
@@ -17,16 +33,16 @@ class User < ActiveRecord::Base
   validates_presence_of     :name
   validates_uniqueness_of   :name
 
-validates_format_of :name,
-   with: RFC822_valid,
-   message: 'must be a valid email address. i.e. user@doman.com'
+  validates_format_of :name,
+    with: RFC822_valid,
+    message: 'must be a valid email address. i.e. user@doman.com'
 
   attr_accessor :password_confirmation
   validates_confirmation_of :password
 
   validate :password_non_blank
 
-#accepts_nested_attributes_for :user_attribute, :allow_destroy => true
+  #accepts_nested_attributes_for :user_attribute, :allow_destroy => true
 
   def self.authenticate(name, password)
     user = self.find_by_name(name)
@@ -44,11 +60,11 @@ validates_format_of :name,
     if self.user_attribute.nil? then
       return(self.name)
     else if self.user_attribute.first_name.nil? or self.user_attribute.last_name.nil? then
-      return(self.name)
-    else
-      return(self.user_attribute.first_name + " " + self.user_attribute.last_name)
+        return(self.name)
+      else
+        return(self.user_attribute.first_name + " " + self.user_attribute.last_name)
+      end
     end
-  end
   end
 
   def create_reset_code
@@ -97,7 +113,7 @@ validates_format_of :name,
     save(false)
   end
 
-def delete_activation_code
+  def delete_activation_code
     @activated=true
     self.attributes = {activation_code: nil, activated_at: Time.now}
     save(false)
@@ -131,14 +147,14 @@ def delete_activation_code
   end
 
 
-def after_destroy
-  if User.count.zero?
-    raise "Can't delete last user"
+  def after_destroy
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
   end
-end
 
 
-private
+  private
 
   def password_non_blank
     errors.add(:password, "Missing password") if hashed_password.blank?
