302774f07ddf04af8d1c591a579018cc2586ecfe
Robert Lee Little III <rob@silverwebsystems.com>
2016-04-15 12:24:19 -0400

    Fixed some bugs in Image Library.

--- app/assets/javascripts/image_libraries/image_list.js
+++ app/assets/javascripts/image_libraries/image_list.js
@@ -6,13 +6,14 @@
 
 var picture_edit_dialog = "";
 var picture_insert_dialog = "";
+var global_editor_hold = "";
 
 $(document).ready(function () {
 
     // alert("in here");
     // $("#tabs").tabs();
     bind_file_upload_to_upload_form();
-    bind_paste_body_zone();
+    // bind_paste_body_zone();
     bind_mouseover();
     initialize_edit_button();
     initialize_insert_image_button();
@@ -38,97 +39,97 @@ function activate_buttons() {
 
 //}
 
-function bind_paste_body_zone()
-{
-    bind_download_to_files();
-    $('body').fileupload({
-        pasteZone: $('body'),
-        dataType: "json",
-        add: function (e, data) {
-            file = data.files[0];
-            data.context = $(tmpl("template-upload", file));
-            // $("div.progress").progressbar();
-            $('#pictures').prepend(data.context);
-            var jqXHR = data.submit()
-                    .success(function (result, statusText, jqXHR) {
-
-                        console.log("------ - fileupload: Success - -------");
-                        console.log(result);
-                        console.log(result.id);
-                        console.log(statusText);
-                        console.log(jqXHR);
-
-                        console.log(JSON.stringify(jqXHR.responseJSON["attachment"]));
-
-                        console.log(typeof (jqXHR.responseText));
-// specifically for IE8. 
-                        if (typeof (jqXHR.responseText) == "undefined") {
-                            setUpPurrNotifier("Notice", jqXHR.responseJSON["attachment"][0]);
-                            data.context.remove();
-                        }
-                        else
-                        {
-                            render_pictures(result.id);
-                           
-                        }
-
-                    })
-                    .error(function (jqXHR, statusText, errorThrown) {
-                        // console.log("------ - fileupload: Error - -------");
-                        // console.log(jqXHR.status);
-                        // console.log(statusText);
-                        // console.log(errorThrown);
-                        // console.log(jqXHR.responseText);
-                        if (jqXHR.status == "200")
-                        {
-                            //   render_pictures();
-                        }
-                        else
-                        {
-                            var obj = jQuery.parseJSON(jqXHR.responseText);
-                            // console.log(typeof obj["attachment"][0])
-                            setUpPurrNotifier("Notice", obj["attachment"][0]);
-                            data.context.remove();
-                        }
-//                        if (jqXHR.statusText == "success") {
-//                            render_pictures();
-//                            // It succeeded and we need to update the file list.
-//                        }
-//                        else {
-//                            var obj = jQuery.parseJSON(jqXHR.responseText);
-//                            setUpPurrNotifier("info.png", "Notice", obj["attachment"][0]);
+//function bind_paste_body_zone()
+//{
+//    bind_download_to_files();
+//    $('body').fileupload({
+//        pasteZone: $('body'),
+//        dataType: "json",
+//        add: function (e, data) {
+//            file = data.files[0];
+//            data.context = $(tmpl("template-upload", file));
+//            // $("div.progress").progressbar();
+//            $('#pictures').prepend(data.context);
+//            var jqXHR = data.submit()
+//                    .success(function (result, statusText, jqXHR) {
+//
+//                        console.log("------ - fileupload: Success - -------");
+//                        console.log(result);
+//                        console.log(result.id);
+//                        console.log(statusText);
+//                        console.log(jqXHR);
+//
+//                        console.log(JSON.stringify(jqXHR.responseJSON["attachment"]));
+//
+//                        console.log(typeof (jqXHR.responseText));
+//// specifically for IE8. 
+//                        if (typeof (jqXHR.responseText) == "undefined") {
+//                            setUpPurrNotifier("Notice", jqXHR.responseJSON["attachment"][0]);
 //                            data.context.remove();
 //                        }
-
-                    })
-                    .complete(function (result, textStatus, jqXHR) {
-                        // console.log("------ - fileupload: Complete - -------");
-                        // console.log(result);
-                        // console.log(textStatus);
-                        // console.log(jqXHR);
-                    });
-        },
-        progress: function (e, data) {
-            if (data.context)
-            {
-                progress = parseInt(data.loaded / data.total * 100, 10);
-                data.context.find('.bar').css('width', progress + '%');
-            }
-        },
-        done: function (e, data) {
-            // console.log(e);
-            // console.log(data);
-            data.context.text('');
-        }
-    }).bind('fileuploaddone', function (e, data) {
-        // console.log(e);
-        // console.log(data);
-        data.context.remove();
-      
-
-        //data.context.text('');
-    });
-}
+//                        else
+//                        {
+//                            render_pictures(result.id);
+//
+//                        }
+//
+//                    })
+//                    .error(function (jqXHR, statusText, errorThrown) {
+//                        // console.log("------ - fileupload: Error - -------");
+//                        // console.log(jqXHR.status);
+//                        // console.log(statusText);
+//                        // console.log(errorThrown);
+//                        // console.log(jqXHR.responseText);
+//                        if (jqXHR.status == "200")
+//                        {
+//                            //   render_pictures();
+//                        }
+//                        else
+//                        {
+//                            var obj = jQuery.parseJSON(jqXHR.responseText);
+//                            // console.log(typeof obj["attachment"][0])
+//                            setUpPurrNotifier("Notice", obj["attachment"][0]);
+//                            data.context.remove();
+//                        }
+////                        if (jqXHR.statusText == "success") {
+////                            render_pictures();
+////                            // It succeeded and we need to update the file list.
+////                        }
+////                        else {
+////                            var obj = jQuery.parseJSON(jqXHR.responseText);
+////                            setUpPurrNotifier("info.png", "Notice", obj["attachment"][0]);
+////                            data.context.remove();
+////                        }
+//
+//                    })
+//                    .complete(function (result, textStatus, jqXHR) {
+//                        // console.log("------ - fileupload: Complete - -------");
+//                        // console.log(result);
+//                        // console.log(textStatus);
+//                        // console.log(jqXHR);
+//                    });
+//        },
+//        progress: function (e, data) {
+//            if (data.context)
+//            {
+//                progress = parseInt(data.loaded / data.total * 100, 10);
+//                data.context.find('.bar').css('width', progress + '%');
+//            }
+//        },
+//        done: function (e, data) {
+//            // console.log(e);
+//            // console.log(data);
+//            data.context.text('');
+//        }
+//    }).bind('fileuploaddone', function (e, data) {
+//        // console.log(e);
+//        // console.log(data);
+//        data.context.remove();
+//
+//
+//        //data.context.text('');
+//    });
+//}
 ;
 
 function bind_mouseover()
@@ -193,6 +194,7 @@ function bind_file_upload_to_upload_form()
 {
     bind_download_to_files();
     $("form#new_picture").fileupload({
+        pasteZone: $('body'),
         dataType: "json",
         add: function (e, data) {
             file = data.files[0];
@@ -219,7 +221,7 @@ function bind_file_upload_to_upload_form()
                         else
                         {
                             render_pictures(result.id);
-                           
+
                         }
 
                     })
@@ -289,7 +291,16 @@ function initialize_insert_button()
             .bind("ajax:success", function (evt, data, status, xhr) {
                 // alert("ajax:success");
                 // edit_picture_dialog(data);
-                top.tinymce.activeEditor.insertContent(data);
+
+                insert_content_picture(data);
+                
+//                if (top.tinymce.activeEditor != null) {
+//                    top.tinymce.activeEditor.insertContent(data);
+//                } else if (window.frames[0].tinymce.activeEditor != null) {
+//                    window.frames[0].tinymce.activeEditor.insertContent(data)
+//                }
+
+
 
             })
             .bind('ajax:complete', function (evt, xhr, status) {
@@ -488,7 +499,30 @@ function edit_picture_dialog(data) {
     //initilize_filter_buttons();
 
 }
+function insert_content_picture(data) {
+//    console.log("----- in insert content picture ----");
+//    console.log(window);
+//    console.log(window.tinyMCE);
+//    console.log(window.document);
+//    console.log(window.parent.window.document);
+//    console.log(window.parent.window.tinyMCE.activeEditor);
+    
+     
+    if (top.tinymce.activeEditor != null) {
+        top.tinymce.activeEditor.insertContent(data);
+        return
+   } 
+   
+    // When in an iframe...this is how you find the active editor.
+    
+     if (window.parent.window.tinyMCE.activeEditor != null) {
+        window.parent.window.tinyMCE.activeEditor.insertContent(data);
+        return
+    }
+  
+//    console.log("----- Exit insert content picture ----");
 
+}
 
 function link_to_picture(picture_id, picture_size) {
     $.ajax({
@@ -500,7 +534,7 @@ function link_to_picture(picture_id, picture_size) {
         {
             console.log(data);
             $(picture_insert_dialog).dialog("close");
-            top.tinymce.activeEditor.insertContent(data);
+            insert_content_picture(data)
 
         },
         error: function (jqXHR, statusText, errorThrown)
@@ -529,7 +563,7 @@ function insert_picture_dialog(data) {
         },
         "Cancel": function cancelClicked() {
             console.log("Cancel.")
-            $(this).dialog("close");
+            picture_insert_dialog.dialog("close");
         }
     }, "Insert,Cancel");
 
