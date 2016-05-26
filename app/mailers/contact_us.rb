f4fe8da4affc178552d75ea95164d5c8ba1f87ea
Robert Lee Little III <rob@silverwebsystems.com>
2016-05-26 00:17:25 -0400

    Autocommit of backout changeset for d865f0ea35fe
    --HG--
    branch : v0.0.2

--- app/mailers/contact_us.rb
+++ app/mailers/contact_us.rb
@@ -8,7 +8,6 @@ class ContactUs < ActionMailer::Base
   #
   
   def send_mail (form_data, from, to, subject)
-    from = Settings.admin_email || "noreply@onewhere.com"
     @form_data=form_data
     @user=from
     puts("stuff", @form_data, from, to, subject)
