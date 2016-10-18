123e596300ea3fd5e6f405eb4f9612ab51397b69
Robert Lee Little III <rob@silverwebsystems.com>
2016-10-18 11:25:56 -0400

    Updated jquery.autogrow with latest version.  Added a call to allow datatable search fields to become more versitile by allowing search by specific fields through dropdown.
    --HG--
    branch : v0.0.2

--- app/assets/javascripts/jquery.autoGrowInput.js
+++ app/assets/javascripts/jquery.autoGrowInput.js
@@ -1,67 +1,47 @@
+/*
+	jQuery autoGrowInput v1.0.3
+    Copyright (c) 2014 Simon Steinberger / Pixabay
+    Based on stackoverflow.com/questions/931207 (James Padolsey)
+    GitHub: https://github.com/Pixabay/jQuery-autoGrowInput
+	License: http://www.opensource.org/licenses/mit-license.php
+*/
+
 (function($){
+    var event = 'oninput' in document.createElement('input') ? 'input' : 'keydown';
 
-$.fn.autoGrowInput = function(o) {
+    $.fn.autoGrowInput = function(options){
+        var o = $.extend({ maxWidth: 500, minWidth: 20, comfortZone: 0 }, options);
 
-    o = $.extend({
-        maxWidth: 1000,
-        minWidth: 0,
-        comfortZone: 70
-    }, o);
-
-    this.filter('input:text').each(function(){
-
-        var minWidth = o.minWidth || $(this).width(),
-            val = '',
-            input = $(this),
-            testSubject = $('<tester/>').css({
-                position: 'absolute',
-                top: -9999,
-                left: -9999,
-                width: 'auto',
-                fontSize: input.css('fontSize'),
-                fontFamily: input.css('fontFamily'),
-                fontWeight: input.css('fontWeight'),
-                letterSpacing: input.css('letterSpacing'),
-                whiteSpace: 'nowrap'
-            }),
-            check = function() {
-
-                if (val === (val = input.val())) {return;}
-
-                // Enter new content into testSubject
-                var escaped = val.replace(/&/g, '&amp;').replace(/\s/g,' ').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-                testSubject.html(escaped);
-
-                // Calculate new width + whether to change
-                var testerWidth = testSubject.width(),
-                    newWidth = (testerWidth + o.comfortZone) >= minWidth ? testerWidth + o.comfortZone : minWidth,
-                    currentWidth = input.width(),
-                    isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
-                                         || (newWidth > minWidth && newWidth < o.maxWidth);
-
-				if (!isValidWidthChange && newWidth > minWidth && newWidth > o.maxWidth) {
-				    newWidth = o.maxWidth;
-				    isValidWidthChange = true;
-				}
-
-                // Animate width
-                if (isValidWidthChange) {
-                    input.width(newWidth);
-                }
-
-            };
-
-        testSubject.insertAfter(input);
-
-        $(this).bind('keyup keydown blur update', check);
-
-        // Auto-size when page first loads
-		check();
-
-    });
-
-    return this;
-
-};
-
-})(jQuery);
+        this.each(function(){
+            var input = $(this),
+                val = ' ',
+                comfortZone = (options && 'comfortZone' in options) ? o.comfortZone : parseInt(input.css('fontSize')),
+                span = $('<span/>').css({
+                    position: 'absolute',
+                    top: -9999,
+                    left: -9999,
+                    width: 'auto',
+                    fontSize: input.css('fontSize'),
+                    fontFamily: input.css('fontFamily'),
+                    fontWeight: input.css('fontWeight'),
+                    letterSpacing: input.css('letterSpacing'),
+                    textTransform: input.css('textTransform'),
+                    whiteSpace: 'nowrap',
+                    ariaHidden: true
+                }).appendTo('body'),
+                check = function(e){
+                    if (val === (val = input.val()) && e.type !== 'autogrow') return;
+                    if (!val) val = input.attr('placeholder') || '';
+                    span.html(val.replace(/&/g, '&amp;').replace(/\s/g, '&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+                    var newWidth = span.width() + comfortZone, mw = typeof(o.maxWidth) == "function" ? o.maxWidth() : o.maxWidth;
+                    if (newWidth > mw) newWidth = mw;
+                    else if (newWidth < o.minWidth) newWidth = o.minWidth;
+                    if (newWidth != input.width()) input.width(newWidth);
+                };
+            input.on(event+'.autogrow autogrow', check);
+            // init on page load
+            check();
+        });
+        return this;
+    }
+}(jQuery));
