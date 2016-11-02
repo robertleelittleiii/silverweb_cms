86c12238761c49564cd06b2032492aeb3b279aab
Robert Lee Little III <rob@silverwebsystems.com>
2016-11-02 13:42:31 -0400

    Fixed bug that was causing the allowed attributes to not update for user attribures and users.
    --HG--
    branch : v0.0.2

--- app/controllers/user_attributes_controller.rb
+++ app/controllers/user_attributes_controller.rb
@@ -85,8 +85,8 @@ class UserAttributesController < ApplicationController
   
   def user_attribute_params
     
-    params[:user_attribute].permit("first_name", "last_name", "handle", "birthdate", "user_id")
-    params[:user_attribute].permit(SilverwebCms::Config.USER_ATTRIBUTE_PERMITTED_FIELDS)
+    params[:user_attribute].permit("first_name", "last_name", "handle", "birthdate", "user_id",SilverwebCms::Config.USER_ATTRIBUTE_PERMITTED_FIELDS)
+    # params[:user_attribute].permit()
 
   end
   
