bd34e5101764b6e9de5847dc90e4b15eb3780d4d
Robert Lee Llittle III <rob@bayshorerecycling.com>
2018-08-15 09:24:58 -0400

    upated code to use new version of index_exists?
    --HG--
    branch : v0.1.0

--- db/migrate/20150209032051_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
+++ db/migrate/20150209032051_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
@@ -25,7 +25,7 @@ class ActsAsTaggableOnMigration < ActiveRecord::Migration[5.0]
         t.datetime :created_at
       end
 
-      add_index :taggings, :tag_id if !ActiveRecord::Base.connection.index_exists? 'index_taggings_on_tag_id'
+      add_index :taggings, :tag_id if !ActiveRecord::Base.connection.index_exists?(:taggings, :tag_id, name: 'index_taggings_on_tag_id')
       add_index :taggings, [:taggable_id, :taggable_type, :context]
     end
   end
