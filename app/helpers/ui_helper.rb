f1de769a30d9c0665cb0b99e5e5be1ac83abc3f7
Robert Lee Little III <rob@silverwebsystems.com>
2018-11-29 01:02:14 -0500

    fixed bug with helper
    --HG--
    branch : v0.0.2

--- app/helpers/ui_helper.rb
+++ app/helpers/ui_helper.rb
@@ -563,11 +563,14 @@ module UiHelper
   #
   #    end
   
-  def create_group_checks_live(field_name, field_pointer, value_list, tag_list_name, html_options={})
+ def create_group_checks_live(field_name, field_pointer, value_list, tag_list_name, html_options={})
     return_value = ""
-    
-    value_list.each do |item|
-      return_value =  return_value + "<div class='div-#{tag_list_name}'>"+ editablecheckboxtag2(item, field_pointer,item,tag_list_name,html_options) +"</div>" 
+    if value_list.class == Array then
+      value_list.each do |item|
+        return_value =  return_value + "<div class='div-#{tag_list_name}'>"+ editablecheckboxtag2(item, field_pointer,item,tag_list_name,html_options) +"</div>" 
+      end
+    else 
+      return_value = "n/a"
     end
     return return_value.html_safe
   end
