0aabe677a0c7ff28f9c544cefd3f566b4372235a
Robert Lee Little III <rob@silverwebsystems.com>
2019-10-21 10:09:34 -0400

    Updated image library to support versions tinymce < 5 and tinymce >= 5.  Different api so need different code.
    --HG--
    branch : v0.1.0

--- app/assets/javascripts/tinymce/plugins/image_library/plugin.js
+++ app/assets/javascripts/tinymce/plugins/image_library/plugin.js
@@ -1,46 +1,93 @@
-tinymce.PluginManager.add('image_library', function(editor, url) {
+tinymce.PluginManager.add('image_library', function (editor, url) {
     global_editor_hold = editor;
-    
+
     // Add a button that opens a window
-    editor.ui.registry.addToggleButton('image_library', {
-        icon: 'image',
-        tooltip: 'Insert/add image from library',
-        onAction: function() {
-            // Open window
-            
-            editor.windowManager.openUrl({
-                title: 'Image Library',
-                url: '/image_library/image_list?as_window=true',
-                width: 875,
-                height: 600,
-                buttons: [{
-                    type: 'cancel',
-                    text: 'Close',
-                    name: 'close'
-                }]
-            });
-        }
-    });
-    
-    
+    if (tinymce.majorVersion < 5) {
+        editor.addButton('image', {
+            icon: 'image',
+            tooltip: 'Insert/add image from library',
+            onAction: function () {
+                // Open window
+
+                editor.windowManager.open({
+                    title: 'Image Library',
+                    url: '/image_library/image_list?as_window=true',
+                    width: 875,
+                    height: 600,
+                    buttons: [{
+                            text: 'Close',
+                            onclick: 'close'
+                        }]
+                });
+            }
+        });
+
+    } else
+    {
+
+        editor.ui.registry.addToggleButton('image_library', {
+            icon: 'image',
+            tooltip: 'Insert/add image from library',
+            onAction: function () {
+                // Open window
+
+                editor.windowManager.openUrl({
+                    title: 'Image Library',
+                    url: '/image_library/image_list?as_window=true',
+                    width: 875,
+                    height: 600,
+                    buttons: [{
+                            type: 'cancel',
+                            text: 'Close',
+                            name: 'close'
+                        }]
+                });
+            }
+        });
+    }
+
 
     // Adds a menu item to the tools menu
-    editor.ui.registry.addMenuItem('image_library', {
-        text: 'Image Library',
-        icon: 'image',
-        onAction: function() {
-            // Open window with a specific url
-            editor.windowManager.openUrl({
-                title: 'Image Library',
-                url: '/image_library/image_list?as_window=true',
-                width: 875,
-                height: 600,
-                buttons: [{
-                    type: 'cancel',
-                    text: 'Close',
-                    name: 'close'
-                }]
-            });
-        }
-    });
+
+
+    if (tinymce.majorVersion < 5) {
+
+        editor.addMenuItem('image', {
+            text: 'Image Library',
+            onAction: function () {
+                // Open window with a specific url
+                editor.windowManager.open({
+                    title: 'Image Library',
+                    url: '/image_library/image_list?as_window=true',
+                    width: 875,
+                    height: 600,
+                    buttons: [{
+                            text: 'Close',
+                            onclick: 'close'
+                        }]
+                });
+            }
+        });
+    } else
+    {
+
+        editor.ui.registry.addMenuItem('image_library', {
+            text: 'Image Library',
+            icon: 'image',
+            onAction: function () {
+                // Open window with a specific url
+                editor.windowManager.openUrl({
+                    title: 'Image Library',
+                    url: '/image_library/image_list?as_window=true',
+                    width: 875,
+                    height: 600,
+                    buttons: [{
+                            type: 'cancel',
+                            text: 'Close',
+                            name: 'close'
+                        }]
+                });
+            }
+        });
+    }
 });
