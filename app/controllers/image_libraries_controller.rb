4c4af28d6c0a4c69825b228885d0ca82e05f5ba4
Robert Lee Little III <rlittle@bayshorerecycling.com>
2021-07-14 14:38:17 -0400

    added autoload and scroll feature to picture library.

--- app/controllers/image_libraries_controller.rb
+++ app/controllers/image_libraries_controller.rb
@@ -1,16 +1,24 @@
 class ImageLibrariesController < ApplicationController
   
   def image_list
-    @pictures = Picture.all.order(created_at: :desc)
-    
-     respond_to do |format|
+    @pictures = Picture.all.order(created_at: :desc).limit(50)
+    session[:page]=1
+    respond_to do |format|
       format.html # image_list.html.erb
-      format.xml  { render xml: @users}
-      format.json { render json: @users}
+      format.xml  { render xml: @pictures}
+      format.json { render json: @pictures}
     end
   end
   
-  
+  def next_images
+    session[:page] = session[:page] + 1
+    @pictures = Picture.page(session[:page]).per(50).order(created_at: :desc).limit(50)
+    respond_to do |format|
+      format.html # image_list.html.erb
+      format.xml  { render xml: @pictures}
+      format.json { render json: @pictures}
+    end
+  end
   
   
   
